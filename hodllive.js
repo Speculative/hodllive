9dbd026236cf1eeee516fc38eb3a44ab74151377
Jeff Tao <jatao88@gmail.com>
2020-10-19 13:07:35 -0400

    Add rank vs. date chart

--- hodllive.js
+++ hodllive.js
@@ -182,7 +182,11 @@ function createGenerationTree() {
   }));
 }
 
-Vue.component("viewership-chart", {
+/*
+Stupid hack to get around the fact that vue-chartjs seems to assume
+that there's only one chart on the page
+*/
+Vue.component("value-chart", {
   extends: VueChartJs.Line,
   mixins: [VueChartJs.mixins.reactiveProp],
   props: ["options"],
@@ -191,7 +195,16 @@ Vue.component("viewership-chart", {
   },
 });
 
-const chartOptions = {
+Vue.component("rank-chart", {
+  extends: VueChartJs.Line,
+  mixins: [VueChartJs.mixins.reactiveProp],
+  props: ["options"],
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+});
+
+const valueChartOptions = {
   responsive: true,
   maintainAspectRatio: false,
   scales: {
@@ -204,6 +217,36 @@ const chartOptions = {
       },
     ],
   },
+  hover: {
+    mode: "point",
+  },
+};
+
+const rankChartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          unit: "day",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        type: "linear",
+        ticks: {
+          stepSize: 1,
+          reverse: true,
+        },
+      },
+    ],
+  },
+  hover: {
+    mode: "point",
+  },
 };
 
 ELEMENT.locale(ELEMENT.lang.en);
@@ -213,15 +256,22 @@ const vChart = new Vue({
     // Hardcoded values
     constants: {
       generationTree: createGenerationTree(),
-      chartOptions,
+      valueChartOptions,
+      rankChartOptions,
     },
     // Computed from hodllive.json
-    viewershipStats: null,
+    viewershipStats: {
+      // map of member -> date -> { views, subs }
+      byMember: null,
+      // map of date -> member -> { views, subs }
+      byDate: null,
+    },
     dateRange: {
       minDate: null,
       maxDate: null,
     },
     // Dynamic configuration values
+    activeTab: "value",
     dimension: "subs",
     selectedMembers: generations["Hololive EN"],
     selectedDateRange: [],
@@ -236,8 +286,16 @@ const vChart = new Vue({
       };
     },
     // Adapt the current configuration to fit the dataset format that chartjs is expecting
-    viewershipChartData() {
-      return getDatasets(
+    valueChartData() {
+      return getValueDatasets(
+        this.viewershipStats,
+        this.selectedMembers,
+        this.selectedDateRange,
+        this.dimension
+      );
+    },
+    rankChartData() {
+      return getRankDatasets(
         this.viewershipStats,
         this.selectedMembers,
         this.selectedDateRange,
@@ -260,8 +318,14 @@ const vChart = new Vue({
         }
       }
     },
+    setActiveTab(index) {
+      console.log(index);
+      this.activeTab = index;
+    },
   },
 });
+
+window.vChart = vChart;
 
 function createDateRangeShortcuts(minDate, maxDate) {
   if (!(minDate && maxDate)) {
@@ -275,7 +339,10 @@ function createDateRangeShortcuts(minDate, maxDate) {
     },
     {
       text: "Past 30 days",
-      onClick: bindSetDateRange(moment(maxDate).subtract(30, "days"), maxDate),
+      onClick: bindSetDateRange(
+        moment(maxDate).subtract(30, "days").toDate(),
+        maxDate
+      ),
     },
   ];
 }
@@ -298,46 +365,82 @@ const dataPromise = fetch("./hodllive.json")
     return response.json();
   })
   .then(function (stats) {
+    // Stats is map of member -> { date, views, subs }[]
     // Produce object from object
     return Object.fromEntries(
-      Object.entries(stats).map(([member, stats]) => [
+      Object.entries(stats).map(([member, memberStats]) => [
         // With same keys,
         member,
-        // But transforming stats
-        stats.map((datapoint) => {
-          // Converting dates
-          let dateComponents = datapoint.date.split("-");
-          let year = Number(dateComponents[0]);
-          let month = Number(dateComponents[1]) - 1; // 0-indexed month
-          let day = Number(dateComponents[2]);
+        // But transforming stats from datapoint[] to (string) date -> datapoint
+        Object.fromEntries(
+          memberStats.map((datapoint) => {
+            const { date, ...rest } = datapoint;
+            // Converting dates
+            let dateComponents = date.split("-");
+            let year = Number(dateComponents[0]);
+            let month = Number(dateComponents[1]) - 1; // 0-indexed month
+            let day = Number(dateComponents[2]);
 
-          return {
-            // And preserving the rest that isn't the date
-            ...datapoint,
-            date: new Date(year, month, day),
-          };
-        }),
+            return [
+              // And preserving the rest that isn't the date
+              `${year}-${month}-${day}`,
+              rest,
+            ];
+          })
+        ),
       ])
     );
   });
-dataPromise.then((stats) => {
-  vChart.viewershipStats = stats;
-  const minDate = Object.values(stats)
-    // Stats are sorted, so the first element is the earliest date
-    .map((datapoints) => datapoints[0])
-    .map((datapoint) => datapoint.date)
-    // Take the minimum of dates
-    .reduce((lowest, current) => (current < lowest ? current : lowest));
+dataPromise.then((statsByMemberByDate) => {
+  vChart.viewershipStats.byMember = statsByMemberByDate;
 
-  const maxDate = Object.values(stats)
-    // Stats are sorted, so the last element is the latest date
-    .map((datapoints) => datapoints[datapoints.length - 1])
-    .map((datapoint) => datapoint.date)
-    // Take the maximum of dates
-    .reduce((highest, current) => (current > highest ? current : highest));
+  const minDate = stringToDate(
+    Object.values(statsByMemberByDate)
+      .map((statsByDate) => Object.keys(statsByDate))
+      // Stats are sorted, so the first element is the earliest date
+      .map((datapoints) => datapoints[0])
+      // Take the minimum of dates
+      .reduce((lowest, current) => (current < lowest ? current : lowest))
+  );
+
+  const maxDate = stringToDate(
+    Object.values(statsByMemberByDate)
+      .map((statsByDate) => Object.keys(statsByDate))
+      // Stats are sorted, so the last element is the latest date
+      .map((datapoints) => datapoints[datapoints.length - 1])
+      // Take the maximum of dates
+      .reduce((highest, current) => (current > highest ? current : highest))
+  );
 
   vChart.dateRange = { minDate, maxDate };
   vChart.selectedDateRange = [minDate, maxDate];
+
+  vChart.viewershipStats.byDate = Object.fromEntries(
+    // For each date
+    dateRange(minDate, maxDate).map((date) => {
+      const stringDate = dateToString(date);
+
+      // Create Object entry
+      return [
+        // With key (string) date
+        stringDate,
+        // And value map of member -> datapoint
+        Object.fromEntries(
+          // Produce another map
+          Object.entries(statsByMemberByDate)
+            .map(([member, statsByDate]) => {
+              return [
+                // With key member
+                member,
+                // And value datapoint at date
+                stringDate in statsByDate ? statsByDate[stringDate] : undefined,
+              ];
+            })
+            .filter(([member, datapoint]) => datapoint !== undefined)
+        ),
+      ];
+    })
+  );
 });
 
 document.getElementById("chart-sidebar-open").onclick = function () {
@@ -348,24 +451,104 @@ document.getElementById("chart-sidebar-close").onclick = function () {
   document.getElementById("chart-config").classList.remove("open");
 };
 
+function dateRange(minDate, maxDate) {
+  const currentDate = moment(minDate);
+  const range = [];
+  while (currentDate.isSameOrBefore(maxDate)) {
+    range.push(currentDate.toDate());
+    currentDate.add(1, "days");
+  }
+  return range;
+}
+
+function dateToString(date) {
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+}
+
+function stringToDate(str) {
+  let dateComponents = str.split("-");
+  let year = Number(dateComponents[0]);
+  let month = Number(dateComponents[1]);
+  let day = Number(dateComponents[2]);
+  return new Date(year, month, day);
+}
+
 // Data Manipulation
 // dateRange: [startDate, endDate]
 // whichStat: "subs" or "views"
-function getDatasets(stats, members, [minDate, maxDate], whichStat) {
+function getValueDatasets(stats, members, [minDate, maxDate], whichStat) {
+  const statsByMemberByDate = stats.byMember;
   return {
     datasets: members.map(function (member) {
-      const datapoints = stats[member];
+      const statsByDate = statsByMemberByDate[member];
       return {
         label: names[member],
-        data: datapoints
-          .filter(
-            (datapoint) =>
-              datapoint.date >= minDate && datapoint.date <= maxDate
-          )
-          .map(function (datapoint) {
+        data: Object.entries(statsByDate)
+          .map(([date, datapoint]) => [stringToDate(date), datapoint])
+          .filter(([date, datapoint]) => date >= minDate && date <= maxDate)
+          .map(function ([date, datapoint]) {
             return {
-              x: datapoint.date,
+              x: date,
               y: datapoint[whichStat],
+            };
+          }),
+        fill: false,
+        borderColor: colors[member],
+        pointBackgroundColor: colors[member],
+        lineTension: 0,
+        pointRadius: 4,
+        borderWidth: 4,
+        pointHoverBorderWidth: 4,
+        pointHoverBackgroundColor: "#000000",
+        pointHoverBorderColor: "#000000",
+      };
+    }),
+  };
+}
+
+// dateRange: [startDate, endDate]
+// whichStat: "subs" or "views"
+function getRankDatasets(stats, members, [minDate, maxDate], whichStat) {
+  const statsByDateByMember = stats.byDate;
+  // Get just the selected members
+  const rankByDateByMember = Object.fromEntries(
+    Object.entries(statsByDateByMember)
+      .map(([date, rankStatsByMember]) => [
+        stringToDate(date),
+        rankStatsByMember,
+      ])
+      .filter(([date, statsByMember]) => date >= minDate && date <= maxDate)
+      .map(([date, statsByMember]) => [
+        dateToString(date),
+        Object.fromEntries(
+          Object.entries(statsByMember)
+            .filter(([member, datapoint]) => members.includes(member))
+            .sort(
+              ([member1, datapoint1], [member2, datapoint2]) =>
+                datapoint2[whichStat] - datapoint1[whichStat]
+            )
+            .map(([member, datapoint], rank) => [
+              member,
+              { ...datapoint, rank },
+            ])
+        ),
+      ])
+  );
+
+  return {
+    datasets: members.map(function (member) {
+      return {
+        label: names[member],
+        data: Object.entries(rankByDateByMember)
+          .map(([date, rankStatsByMember]) => [
+            stringToDate(date),
+            rankStatsByMember,
+          ])
+          .filter(([date, rankByMember]) => member in rankByMember)
+          .map(function ([date, rankByMember]) {
+            return {
+              x: date,
+              y: rankByMember[member].rank + 1, // 0-indexed ranks are bad for humans
             };
           }),
         fill: false,
